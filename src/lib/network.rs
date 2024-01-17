4cab2e068726546312988b8b97860a3c6b8c68c3
AvaterClasher <avater.clasher@gmail.com>
2024-01-17 16:51:37 +0530

    feat: :sparkles: impl Activation functions

--- src/lib/network.rs
+++ src/lib/network.rs
@@ -1,29 +1,49 @@
+use super::activations::{self, Activation};
 use super::matrix::Matrix;
 
-pub struct Network {
+pub struct Network<'a> {
     layers: Vec<usize>,
     weights: Vec<Matrix>,
     biases: Vec<Matrix>,
     data: Vec<Matrix>,
+    activation: Activation<'a>,
 }
 
-impl Network {
-    pub fn new(layers: Vec<usize>) -> Network {
+impl Network<'_> {
+    pub fn new<'a>(layers: Vec<usize>, activation: Activation<'a>) -> Network {
         let mut weights = vec![];
-        let mut biases= vec![];
+        let mut biases = vec![];
 
         for i in 0..layers.len() - 1 {
             weights.push(Matrix::random(layers[i + 1], layers[i]));
-            biases.push(Matrix::random(layers[i+1], 1));
+            biases.push(Matrix::random(layers[i + 1], 1));
         }
 
-        Network{
+        Network {
             layers,
             weights,
             biases,
             data: vec![],
+            activation,
         }
     }
 
-    
+    pub fn feed_forward(&mut self, inputs: Vec<f64>) -> Vec<f64> {
+        if inputs.len() != self.layers[0] {
+            panic!("invalid number of inputs");
+        }
+
+        let mut current = Matrix::from(vec![inputs]).transpose();
+        self.data = vec![current.clone()];
+
+        for i in 0..self.layers.len() - 1 {
+            current = self.weights[i]
+                .multiply(&current)
+                .add(&self.biases[i])
+                .map(self.activation.function);
+            self.data.push(current.clone());
+        }
+
+        
+    }
 }
